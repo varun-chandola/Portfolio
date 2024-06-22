e3db6d4784d736ca8d56600fd344462f13de0e4f
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-06-22 00:06:04 +0000

    App layout updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,3 @@
-//     --color-body: #b6cbce;
-// --color-heading: #eef3db;
-// --color-base: #033f47;
-// --color-base2: #022a30;
-// --color-brand: #e0f780;
-// --color-brand2: #deff58;
-// --sidbar-width: 240px;
-// --font-base: "Bai Jamjuree";
-
-// background-color: var(--color-base2);
-// color: var(--color-body);
-// font-family: var(--font-base), sans-serif;
-// }
-
 import "./index.css";
 import About from "./About"
 import Home from "./Home"
