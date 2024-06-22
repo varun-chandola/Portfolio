8ffb9cc9a469f37cbad720e803fdecfb0393a323
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-06-22 00:06:17 +0000

    contact layour changes

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,24 +8,24 @@ const Contact = () => {
       </div>
       <div
         id="contactMe"
-        className="my-[30px] socials flex justify-around"
+        className="my-10 socials flex flex-wrap m-auto w-auto justify-center"
       >
-        <div className="hovering m-2 rounded-full overflow-hidden">
+        <div className="hovering rounded-full overflow-hidden">
           <a href="#" target="_blank">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwH--J-ZMUg8puNfUxE6YXQi3yVHuAORDxow&s" />
           </a>
         </div>
-        <div className="hovering m-2 rounded-2xl">
+        <div className="hovering rounded-2xl">
           <a href="mailto:varun7122001@gmail.com">
             <img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png" />
           </a>
         </div>
-        <div className="hovering m-2 rounded-full overflow-hidden">
+        <div className="hovering rounded-full overflow-hidden">
           <a href="https://www.linkedin.com/in/varun-chandola/" target="_blank">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/480px-LinkedIn_logo_initials.png" />
           </a>
         </div>
-        <div className="hovering m-2 rounded-full overflow-hidden">
+        <div className="hovering rounded-full overflow-hidden">
           <a href="https://x.com/VarunChandola7" target="_blank">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/X_logo.jpg/1200px-X_logo.jpg" />
           </a>
