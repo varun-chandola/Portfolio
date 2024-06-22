584ac7770fa1e72adff1a94a3dcef56490af74cb
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-06-22 00:06:30 +0000

    resume updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,21 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="flex sm:(flex h-screen)">
-        <div className="m-10 sm:(w-[50%] border-white flex flex-col justify-center items-center flex-col) ">
-          <h1 className="info text-4xl sm:(text-[130px] text-white)">
-            Hi , I am
-          </h1>
-          <h1 className="head text-[50px] sm:(text-[90px] head )">
-            Varun Chandola
-          </h1>
-          <p className="info text-2xl p-10 sm:(info text-[30px] mx-[20px]) "><br/>
-            I am a <span className="head">FrontEnd developer</span>! Upskilling to become a <br/>
+      <div className="mb-20 mt-10 lg:flex xl:h-screen">
+        <div className="flex flex-col items-center m-3 lg:w-1/2 xl:py-36">
+          <h1 className="info text-2xl">Hi , I am</h1>
+          <h1 className="head text-4xl">Varun Chandola</h1>
+          <p className="info flex flex-col text-2xl items-center lg:text-3xl">
+            <br />I am a <span className="head">FrontEnd developer !</span>
+            Upskilling to become a <br />
             Full Stack Developer
           </p>
         </div>
-        <div className="hovering rounded-full overflow-hidden m-10">
-          <img src="varun.jpg" width={400} />
+        <div className="hovering rounded-full overflow-hidden m-auto h-60 w-48 lg:h-4/5 lg:w-auto lg:rounded-2xl">
+          <img src="varun.jpg" className="w-full h-full object-cover lg:h-[600px]" />
         </div>
       </div>
     </>
@@ -25,16 +22,3 @@ const Home = () => {
 };
 
 export default Home;
-
-/**
- * <div className="flex h-screen">
-    <div className='w-[50%] border-white flex justify-center items-center flex-col'>
-        <h1 className='text-[130px] text-white'>Hi , I am</h1>
-        <h1 className='text-[90px] head '>Varun Chandola</h1>
-        <p className='info text-[30px]'>I am a FrontEnd developer !<br /> Upskilling to become a Full Stack Developer</p>
-    </div>
-    <div className='hovering'>
-            <img src="varun.jpg" width={500}/>
-    </div>
-</div>
- */
